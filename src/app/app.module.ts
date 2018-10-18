c81223c284aa12744da8c1f46591d01602bb19ef
Vikhyath <vikhyatc@thoughtworks.com>
2018-10-18 15:05:43 +0530

    [Viky|Sri] Added door component to home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DoorComponent } from './door/door.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, DoorComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/door/door.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'door',
+	templateUrl: 'door.component.html',
+	styleUrls: ['door.component.css']
+})
+
+export class DoorComponent;
