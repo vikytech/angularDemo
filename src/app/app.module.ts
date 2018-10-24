f5894e73f95261b740898bf143d3ba8138ee91a3
Vikhyath <vikhyatc@thoughtworks.com>
2018-10-24 10:54:31 +0530

    [Sri|Viky] Added Side-bar using ngif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DoorComponent } from './door/door.component';
 import { MainDoorComponent } from './main-door/main-door.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent, DoorComponent, MainDoorComponent
+    AppComponent, DoorComponent, MainDoorComponent, SideBarComponent
   ],
   imports: [
     BrowserModule
